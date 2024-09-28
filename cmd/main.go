fc1d969313209bc3d67583bfa866b700f573d1d7
HONG-JUNG-WAN <hong43ok@gmail.com>
2024-09-28 21:59:29 +0900

    feat: main.go 구현

---
+++ cmd/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/HongJungWan/commerce-system/internal/helper"
+	configs "github.com/HongJungWan/commerce-system/internal/infrastructure/configs"
+	"github.com/HongJungWan/commerce-system/internal/infrastructure/router"
+	"github.com/spf13/viper"
+	"gorm.io/gorm"
+	"net/http"
+	"os"
+)
+
+var (
+	conf = configs.Config{}
+	file string
+)
+
+func main() {
+	if !parseConfig() {
+		helper.ShowHelp()
+		os.Exit(-1)
+	}
+	db := configs.ConnectionDB(&conf)
+	startServer(db)
+}
+
+func loadConfig() bool {
+	_, err := os.Stat(file)
+	if err != nil {
+		return false
+	}
+
+	viper.SetConfigFile(file)
+	viper.SetConfigType("toml")
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+	fmt.Println(conf.DBHost)
+	err = viper.GetViper().Unmarshal(&conf)
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+	return true
+}
+
+func parseConfig() bool {
+	flag.StringVar(&file, "c", "config.toml", "config file")
+	flag.Parse()
+	if !loadConfig() {
+		return false
+	}
+
+	return true
+}
+
+func startServer(db *gorm.DB) {
+	routers := router.NewRouter(conf, db)
+
+	server := &http.Server{
+		Addr:    ":3031",
+		Handler: routers,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		helper.ErrorPanic(err)
+	}
+}
