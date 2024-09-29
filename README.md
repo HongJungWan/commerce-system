deaa471a8b11e549ca71071aeec7b7c485496c79
HongJungWan <76596316+HongJungWan@users.noreply.github.com>
2024-09-29 10:55:17 +0900

    fix: infra 설정 수정 (#5)
    * fix: infra 설정 수정
    
    - compose.yml
    - config.toml
    - router.go
    
    * docs: README.md

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # commerce-system
+
+실행 방법
+1. `docker-compose up` 명령어 실행
+2. `docker build --no-cache -t my-app-image .` 명령어 실행
--- internal/infrastructure/router/router.go
+++ internal/infrastructure/router/router.go
@@ -1,14 +1,12 @@
 package router
 
 import (
-	domain "github.com/HongJungWan/commerce-system/internal/domain"
+	"github.com/HongJungWan/commerce-system/internal/domain"
 	"github.com/HongJungWan/commerce-system/internal/infrastructure/configs"
 	controller "github.com/HongJungWan/commerce-system/internal/interfaces/controller"
 	"github.com/HongJungWan/commerce-system/internal/usecases"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	swaggerFiles "github.com/swaggo/files"
-	ginSwagger "github.com/swaggo/gin-swagger"
 	"gorm.io/gorm"
 	"net/http"
 )
@@ -24,15 +22,13 @@ func NewRouter(conf configs.Config, db *gorm.DB) *gin.Engine {
 
 	service.Use(cors.New(config))
 	service.GET("", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, "ffmpeg video modules")
+		ctx.JSON(http.StatusOK, "commerce system")
 	})
 
-	// 데이터베이스 마이그레이션
+	// 데이터베이스 마이그레이션, 일단 막아 두기
 	db.Table("member").AutoMigrate(&domain.Member{})
 	db.Table("order").AutoMigrate(&domain.Order{})
 	db.Table("product").AutoMigrate(&domain.Product{})
-
-	service.Static("/downloads", "./downloads")
 
 	// Health Check 관련 설정
 	healthCheckInteractor := usecases.NewHealthCheckInteractor()
@@ -44,9 +40,6 @@ func NewRouter(conf configs.Config, db *gorm.DB) *gin.Engine {
 
 	// API 라우트 설정
 	router.GET("/health", healthCheckController.HealthCheck)
-
-	// Swagger UI 라우트 추가
-	service.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	return service
 }
