5ad3257f239afd18f6513d819f448abc3575790e
HONG-JUNG-WAN <hong43ok@gmail.com>
2024-09-29 13:55:04 +0900

    feat: 상품 API 구현
    - 상품 조회 API 구현
    - 상품 등록 API 구현
    - 상품 재고 수량 수정 API 구현
    - 상품 삭제 API 구현

--- internal/domain/repository/product_repository.go
+++ internal/domain/repository/product_repository.go
@@ -1,4 +1,11 @@
 package repository
 
+import "github.com/HongJungWan/commerce-system/internal/domain"
+
 type ProductRepository interface {
+	Create(product *domain.Product) error
+	GetAll(filter map[string]interface{}) ([]*domain.Product, error)
+	GetByProductNumber(productNumber string) (*domain.Product, error)
+	Update(product *domain.Product) error
+	Delete(productNumber string) error
 }
--- internal/usecases/product_interactor.go
+++ internal/usecases/product_interactor.go
@@ -1,1 +1,58 @@
 package usecases
+
+import (
+	"errors"
+
+	"github.com/HongJungWan/commerce-system/internal/domain"
+	"github.com/HongJungWan/commerce-system/internal/domain/repository"
+	"gorm.io/gorm"
+)
+
+type ProductInteractor struct {
+	ProductRepository repository.ProductRepository
+	DB                *gorm.DB
+}
+
+func NewProductInteractor(repo repository.ProductRepository, db *gorm.DB) *ProductInteractor {
+	return &ProductInteractor{
+		ProductRepository: repo,
+		DB:                db,
+	}
+}
+
+func (pi *ProductInteractor) CreateProduct(product *domain.Product) error {
+	if err := product.Validate(); err != nil {
+		return err
+	}
+	return pi.ProductRepository.Create(product)
+}
+
+func (pi *ProductInteractor) GetProducts(filter map[string]interface{}) ([]*domain.Product, error) {
+	return pi.ProductRepository.GetAll(filter)
+}
+
+func (pi *ProductInteractor) UpdateStock(productNumber string, quantity int) error {
+	product, err := pi.ProductRepository.GetByProductNumber(productNumber)
+	if err != nil {
+		return err
+	}
+	if err := product.UpdateStock(quantity); err != nil {
+		return err
+	}
+	return pi.ProductRepository.Update(product)
+}
+
+func (pi *ProductInteractor) DeleteProduct(productNumber string) error {
+	product, err := pi.ProductRepository.GetByProductNumber(productNumber)
+	if err != nil {
+		return err
+	}
+	canBeDeleted, err := product.CanBeDeleted(pi.DB)
+	if err != nil {
+		return err
+	}
+	if !canBeDeleted {
+		return errors.New("주문된 이력이 있어 삭제할 수 없습니다.")
+	}
+	return pi.ProductRepository.Delete(productNumber)
+}
